6e7620412dfdd309414eeadf786e17d72a722527
Lance Snider <lance@lancesnider.com>
2016-07-23 11:18:44 -0600

    Add styled board

--- math-boggle/GameBoard.js
+++ math-boggle/GameBoard.js
@@ -3,7 +3,43 @@ import React from 'react'
 class Operands extends React.Component {
   render(){
     return (
-      <div>Operands</div>
+      <div className="operands">
+        <div>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+        </div>
+        <div>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+        </div>
+        <div>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+        </div>
+        <div>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+        </div>
+        <div>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+          <button className="button">9</button>
+        </div>
+      </div>
     )
   }
 }
@@ -11,7 +47,16 @@ class Operands extends React.Component {
 class Operators extends React.Component {
   render(){
     return (
-      <div>Operators</div>
+      <div>
+        <div className="operators">
+          <button className="button">+</button>
+          <button className="button">-</button>
+          <button className="button">*</button>
+          <button className="button">/</button>
+          <button className="button">^</button>
+        </div>
+        <button className="button equals">=</button>
+      </div>
     )
   }
 }
