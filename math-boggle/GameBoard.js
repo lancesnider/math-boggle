dfa7dd588b1d602118eaf4ca525037b1a39cdfcb
Lance Snider <lance@lancesnider.com>
2016-07-23 12:34:55 -0600

    Add random numbers to board

--- math-boggle/GameBoard.js
+++ math-boggle/GameBoard.js
@@ -1,48 +1,61 @@
 import React from 'react'
 
 class Operands extends React.Component {
+  constructor(){
+    super()
+    this.arrayOfRandomNumbers = this.arrayOfRandomNumbers.bind(this)
+  }
+
+  componentWillMount(){
+    this.arrayOfRandomNumbers(5, 5)
+  }
+
+  arrayOfRandomNumbers(rows, columns){
+    var numbersArray = []
+    for (var i = 0; i < rows; i++) {
+      var numbersInRow = []
+      for (var j = 0; j < rows; j++) {
+        var randomNumber = Math.floor(Math.random() * 10)
+        numbersInRow.push(randomNumber)
+      }
+      numbersArray.push(numbersInRow)
+    }
+    this.setState({
+      randomNumbers: numbersArray
+    })
+  }
+
   render(){
+    var rows = this.state.randomNumbers.map(function(number, key){
+      return (
+        <div key={key} >
+          <OperandsRow rowNumbers={number} />
+        </div>
+      )
+    })
     return (
       <div className="operands">
-        <div>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-        </div>
-        <div>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-        </div>
-        <div>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-        </div>
-        <div>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-        </div>
-        <div>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-          <button className="button">9</button>
-        </div>
+        {rows}
       </div>
     )
   }
 }
+
+class OperandsRow extends React.Component {
+  render(){
+    var operands = this.props.rowNumbers.map(function(operand, key){
+      return (
+        <button key={key} className="button">{operand}</button>
+      )
+    })
+    return (
+      <div>
+        {operands}
+      </div>
+    )
+  }
+}
+
 
 class Operators extends React.Component {
   render(){
